e6043e58a1a4cee374b6f127aec5b62f8555a369
Foster Brereton <fbrereto@adobe.com>
2023-03-20 08:25:30 -0700

    Tweaking compiler pragmas yet again.

--- include/_clang_include_prefix.hpp
+++ include/_clang_include_prefix.hpp
@@ -1,14 +1,9 @@
-#pragma clang diagnostic push
-
-#pragma clang diagnostic ignored "-Wunknown-pragmas"
-
-#pragma clang diagnostic ignored "-Wdeprecated-anon-enum-enum-conversion"
-#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
-#pragma clang diagnostic ignored "-Wc++98-compat"
-#pragma clang diagnostic ignored "-Wold-style-cast"
-#pragma clang diagnostic ignored "-Wpadded"
-#pragma clang diagnostic ignored "-Wreserved-identifier"
-#pragma clang diagnostic ignored "-Wshadow-field-in-constructor"
-#pragma clang diagnostic ignored "-Wshorten-64-to-32"
-#pragma clang diagnostic ignored "-Wsign-conversion"
-#pragma clang diagnostic ignored "-Wunused-parameter"
+#if __clang__
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wunknown-pragmas"
+    #pragma clang diagnostic ignored "-Wall"
+#elif __GNUC__ // must follow clang (which defines both)
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wunknown-pragmas"
+    #pragma GCC diagnostic ignored "-Wall"
+#endif
--- include/_clang_include_suffix.hpp
+++ include/_clang_include_suffix.hpp
@@ -1,1 +1,5 @@
-#pragma clang diagnostic pop
+#if __clang__
+    #pragma clang diagnostic pop
+#elif __GNUC__ // must follow clang (which defines both)
+    #pragma GCC diagnostic pop
+#endif
