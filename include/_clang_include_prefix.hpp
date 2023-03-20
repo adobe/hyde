217c1ddfe26ba67b0f92c701a18c1a9f778c30c6
Foster Brereton <fbrereto@adobe.com>
2023-03-20 14:17:18 -0700

    Adding `-Weverything` to clang

--- include/_clang_include_prefix.hpp
+++ include/_clang_include_prefix.hpp
@@ -2,6 +2,7 @@
     #pragma clang diagnostic push
     #pragma clang diagnostic ignored "-Wunknown-pragmas"
     #pragma clang diagnostic ignored "-Wall"
+    #pragma clang diagnostic ignored "-Weverything"
 #elif __GNUC__ // must follow clang (which defines both)
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wunknown-pragmas"
